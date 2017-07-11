77c35e403095134ce86638f101b981580621f8d7
AntonKarabaza <antonkarabaza@google.com>
2017-07-11 16:23:57 +0300

    Modify connect_via_key() to use unpacked dict
    In class SshConnection optimize method connect_via_key() to create
    connection using unpacked dict.
    
    Affects #12

--- ssh_automated_tests/core/connection.py
+++ ssh_automated_tests/core/connection.py
@@ -13,29 +13,24 @@ class Connection(ABC):
 
 class SshConnection:
     def __init__(self, user: User):
-        self.client = paramiko.SSHClient()
-        self.user = user
+        self._client = paramiko.SSHClient()
+        self._user = user
 
     def connect_via_key(self):
         print('connecting')
-        connection_data = self.user.connection_data()
-        pkey = paramiko.RSAKey.from_private_key_file(connection_data["key"])
-        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.client.connect(hostname=connection_data["hostname"],
-                            username=connection_data["username"],
-                            port=connection_data["port"],
-                            pkey=pkey)
+        self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self._client.connect(**self._user.connection_data())
         return self
 
     def open_shell(self):
         print('invoking shell')
-        return self.client.invoke_shell()
+        return self._client.invoke_shell()
 
     def open_sftp(self):
         print('invoking sftp')
-        return self.client.open_sftp()
+        return self._client.open_sftp()
 
     def close(self):
-        if self.client is not None:
+        if self._client is not None:
             print('closing')
-            self.client.close()
+            self._client.close()
