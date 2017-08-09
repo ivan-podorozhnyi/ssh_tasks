ef6e456c902209e61748bdd9dd229d43d861358d
AntonKarabaza <antonkarabaza@google.com>
2017-08-09 16:57:04 +0300

    Modify SshConnection to implement all interface methods
    Add interface SshConnection extending Connection interface.
    Rename SshConnection class to BaseSshConnection and implement
    SshConnection interface.
    
    Affects #11

--- ssh_automated_tests/core/connection.py
+++ ssh_automated_tests/core/connection.py
@@ -16,25 +16,31 @@ class Connection(ABC):
 
 
 class SshConnection(Connection):
+    @abstractmethod
+    def open_shell(self):
+        pass
+
+    @abstractmethod
+    def open_sftp(self):
+        pass
+
+
+class BaseSshConnection(SshConnection):
     def __init__(self, user: User):
         self._client = paramiko.SSHClient()
         self._user = user
 
     def connect(self):
-        print('connecting')
         self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self._client.connect(**self._user.connection_data())
         return self
 
     def open_shell(self):
-        print('invoking shell')
         return self._client.invoke_shell()
 
     def open_sftp(self):
-        print('invoking sftp')
         return self._client.open_sftp()
 
     def disconnect(self):
         if self._client is not None:
-            print('closing')
             self._client.close()
